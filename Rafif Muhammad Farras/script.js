0dfed8194ddbb01b9de33a8737b090c6b31071ad
Raphcel <103039140+Raphcel@users.noreply.github.com>
2024-08-19 21:38:12 +0700

    Add files via upload
    Mengupload penugasan About me 101

---
+++ Rafif Muhammad Farras/script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+
+  <script src="https://kit.fontawesome.com/6fd737f303.js" crossorigin="anonymous"></script>
